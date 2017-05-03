49b0fe27a887cc6e9026bfc1223f9f1d8b6a59e9
Andrii Artamonov <artamonov06@gmail.com>
2017-05-03 18:03:17 +0200

    Added SharedPreferences for all markers with corresponding information

--- app/src/main/java/com/example/artam/mapspolygons/MapsActivity.java
+++ app/src/main/java/com/example/artam/mapspolygons/MapsActivity.java
@@ -39,15 +39,14 @@ import static com.example.artam.mapspolygons.R.id.add;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
-
     private GoogleMap mMap;
-    SharedPreferences sPref = null;
-
-    Marker marker = null;
-    String myLocation;
-    Set <String> markersSet;
-
+    int markersCount;
     final String TAG = "my_logs";
+
+    SharedPreferences sPref = null;
+    Marker marker = null;
+    String myLocation, markersCountStr;
+    LatLng position1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,21 +57,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        Log.i(TAG, "Activity was created");
-
-
-
     }
-
 
     @Override
     public void onMapLongClick(LatLng latLng) {
+        Log.i(TAG, "onMapLongClick");
 
         EditText editT = (EditText) findViewById(R.id.editText);
         myLocation = editT.getText().toString();
-
-        markersSet = new HashSet<>();
-        markersSet.add(latLng.toString());
 
         MarkerOptions options = new MarkerOptions()
                 .position(latLng)
@@ -84,53 +76,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Toast.makeText(getApplicationContext(),
                 "New marker added \n" + latLng.toString(), Toast.LENGTH_LONG)
                 .show();
-//http://stackoverflow.com/questions/22817902/android-g-maps-markers-how-to-store-the-icon-in-shared-preferences
-        sPref.edit().putString("Lat",String.valueOf(latLng.latitude)).apply();
-        sPref.edit().putString("Lng",String.valueOf(latLng.longitude)).apply();
+
+        markersCount++;
+        markersCountStr = Integer.toString(markersCount);
+        Toast.makeText(getApplicationContext(), markersCountStr, Toast.LENGTH_SHORT).show();
+
+        // http://stackoverflow.com/questions/22814490/how-to-save-a-marker-onmapclick
+        sPref.edit().putString("Lat" + Integer.toString((markersCount - 1)), Double.toString(latLng.latitude)).apply();
+        sPref.edit().putString("Lng" + Integer.toString((markersCount - 1)), Double.toString(latLng.longitude)).apply();
+        sPref.edit().putString("myLocation", myLocation).apply();
+        sPref.edit().putInt("markersCount", markersCount).apply();
+    }
+
+    public void extractMarkers() { //http://stackoverflow.com/questions/35868807/saving-google-map-markers-into-sharedpreferences-in-android-studios
+
+        Log.i(TAG, "extractMarkers");
+        sPref = this.getSharedPreferences("location", 0);
+        Log.i(TAG, "extractMarkers - getSharedPreferences");
+        markersCount = sPref.getInt("markersCount", 0);
+        Log.i(TAG, "extractMarkers - get markersCount");
+
+        if (markersCount != 0) {
+            Log.i(TAG, "extractMarkers");
+            String lat = "";
+            String lng = "";
+            String myLocation = "";
+
+            // Iterating through all the locations stored
+            for (int i = 0; i < markersCount; i++) {
+                Log.i(TAG, "extractMarkers - in for loop");
+                // Getting the latitude of the i-th location
+                lat = sPref.getString("Lat" + i, "0");
+                Log.i(TAG, "extractMarkers - get lat");
+                // Getting the longitude of the i-th location
+                lng = sPref.getString("Lng" + i, "0");
+                Log.i(TAG, "extractMarkers - get lng");
+
+                myLocation = sPref.getString("myLocation", "0");
+
+                Toast.makeText(this, "Location: " + myLocation + "\n " + "Latitude: " + lat + "\n "
+                        + "Longitude: " + lng, Toast.LENGTH_LONG).show();
+
+                double lat3 = Double.valueOf(lat);
+                double lng3 = Double.valueOf(lng);
+
+                position1 = new LatLng(lat3, lng3);
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(position1)
+                        .title(myLocation)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+                Log.i(TAG, "extractMarkers - I am done");
             }
-
-        public void storeMarkers() {
-           /* sPref = getPreferences(MODE_PRIVATE);
-            SharedPreferences.Editor editor = sPref.edit();
-            editor.putStringSet("markers", markersSet);
-            editor.apply();*/
-            }
-
-    public void extractMarkers() { //http://stackoverflow.com/questions/22814490/how-to-save-a-marker-onmapclick
-
-        sPref = this.getSharedPreferences("LatLng", MODE_PRIVATE);
-        //Check whether your preferences contains any values then we get those values
-        if ((sPref.contains("Lat")) && (sPref.contains("Lng"))) {
-            String lat = sPref.getString("Lat", "");
-            String lng = sPref.getString("Lng", "");
-            LatLng l = new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
-
-            mMap.addMarker(new MarkerOptions()
-                    .position(l));
         }
     }
 
-    private boolean checkReady() { //Google Maps Android Samples
-        if (mMap == null) {
-            Toast.makeText(this, R.string.map_not_ready, Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        return true;
-    }
-
-    /** Called when the Clear button is clicked. */
+    /**
+     * Called when the Clear button is clicked.
+     */
 
     public void onClearMarkers(View view) { //Google Maps Android Samples
+        Log.i(TAG, "onClearMarkers");
 
-        if (!checkReady()) {
-            return;
-        }
         mMap.clear();
+        sPref.edit().clear().apply();
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        Log.i(TAG, "in onMapReady");
+        Log.i(TAG, "onMapReady");
         mMap = googleMap;
         extractMarkers();
         mMap.setOnMapLongClickListener(this);
@@ -150,26 +166,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         mMap.setMyLocationEnabled(true);
-        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener(){
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
             @Override
             public boolean onMyLocationButtonClick() {
-                if (isGPSEnabled(getApplicationContext())){
+                if (isGPSEnabled(getApplicationContext())) {
                     return false;
                 } else Toast.makeText(getApplicationContext(),
                         "Please enable your GPS for detecting your current position!",
                         Toast.LENGTH_LONG).show();
                 return true;
-                                }
+            }
         });
     }
 
-    public boolean isGPSEnabled (Context mContext){   //http://stackoverflow.com/questions/843675/how-do-i-find-out-if-the-gps-of-an-android-device-is-enabled
+    public boolean isGPSEnabled(Context mContext) {   //http://stackoverflow.com/questions/843675/how-do-i-find-out-if-the-gps-of-an-android-device-is-enabled
         LocationManager locationManager = (LocationManager)
                 mContext.getSystemService(Context.LOCATION_SERVICE);
 
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
-          }
+}
 
 
 
